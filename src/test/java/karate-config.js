e5c19a57541693063a6bcbbdc66f740813b384aa
calvario31 <cvargasr.31@gmail.com>
2021-09-12 00:02:23 -0500

    update karate-config

--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -6,7 +6,7 @@ function fn() {
     var result = karate.callSingle('classpath:Register.feature', config)
     config.token = result.token
     config.credentials = result.credentials
-    karate.configure('headers', {'Content-Type': 'application/json'})
+    karate.configure('headers', {ContentType: 'application/json'})
     karate.configure('headers', {Accept: 'application/json'})
 
     return config;
